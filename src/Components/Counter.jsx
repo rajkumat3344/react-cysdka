b23f7b5b57498290fc6563b241c88425c808cbe8
rajkumat3344 <rajkumarsindri22@gmail.com>
2021-08-21 16:15:44 +0530

    2 files modified

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -9,21 +9,34 @@ export default class Counter extends Component {
     this.state = {
       counter: 0
     };
-    this.increment = this.increment.bind(this);
+    // this.increment = this.increment.bind(this);
+    // this.decrement = this.decrement.bind(this);
   }
 
-  render() {
+  render = () => {
     return (
       <div className="counter">
-        <button onClick={this.increment}>+1</button>
+        <button className="btn1" onClick={this.increment}>
+          +1
+        </button>
         <span className="count">{this.state.counter}</span>
+        <button className="btn2" onClick={this.decrement}>
+          -1
+        </button>
       </div>
     );
-  }
-  increment() {
+  };
+  increment = () => {
     //update state --> counter++
     this.setState({
       counter: this.state.counter + 1
     });
-  }
+  };
+
+  decrement = () => {
+    //update state --> counter--
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  };
 }
