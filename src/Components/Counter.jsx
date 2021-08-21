2d343d1c95cd873ffb90a5d5d3e1b4a090068e32
rajkumat3344 <rajkumarsindri22@gmail.com>
2021-08-21 15:52:00 +0530

    1 file modified

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -21,8 +21,8 @@ export default class Counter extends Component {
   }
   increment() {
     //update state --> counter++
-    this.setState = {
+    this.setState({
       counter: this.state.counter + 1
-    };
+    });
   }
 }
